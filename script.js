e4914b05ecad793c027def8f86f9dd53b21f8485
lab-machine <ahasanhabib51971@gmail.com>
2025-01-05 17:29:43 +0600

    log in button added

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+
+function handleClick(){
 const logButton = document.getElementById("loginButton");
 const oput = document.getElementById("output");
 
 
 logButton.addEventListener('click',()=>{
-    oput.textContent = "You Have successfully Logged in!"
+    // oput.textContent = "You Have successfully Logged in!"
+    oput.textContent='Logged in............';
 });
+
+}
+
+handleClick();
